213f4bfb5589054ef14ebf4d69ecd2493c8cc50a
Megha sankannavar <sankannavarmegha98@gmail.com>
2023-01-04 14:08:44 +0530

    fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,8 @@ import { Component, React } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle,Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Col, Label} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -10,7 +12,7 @@ const minLength = (len) => (val) => val && (val.length >= len);
 function RenderDish({dish}) {
         return (
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
@@ -19,7 +21,7 @@ function RenderDish({dish}) {
         );
     }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId}) {
         if (comments != null)
             return (
                 <div>
@@ -34,7 +36,7 @@ function RenderComments({comments}) {
                             );
                         })}
                     </ul>
-                    <CommentForm />
+                    <CommentForm dishId={dishId} addComment={addComment} />
                 </div>  
             );
         else
@@ -62,7 +64,10 @@ class CommentForm extends Component {
             isModalOpen: !this.state.isModalOpen
         });
         }
-    
+    handleSubmit(values) {
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    }
     validate(author) {
         const errors = {
             author: ''
@@ -139,6 +144,24 @@ class CommentForm extends Component {
 }
 
 const DishDetail = (props) => {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
     if (props.dish != null) {
     return (
         <div className="container">
@@ -157,7 +180,10 @@ const DishDetail = (props) => {
                     <RenderDish dish={props.dish} />
                 </div>
                 <div className='col-12 col-md-5 m-1'>
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments} 
+                    addComment={props.addComment}
+                    dishId={props.dish.id}
+                    />
                 </div>
             </div>
         </div>
